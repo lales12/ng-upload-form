cd7bb5a109d9f58b06dac91d234d3e58e42df9e3
alex <alexander@incubio.com>
2014-02-25 13:15:27 +0100

    new update 0.2

--- ng-jquery-form.js
+++ ng-jquery-form.js
@@ -9,7 +9,7 @@
         function(){
             return {
                 restrict: 'EA',
-                template: "<input type='file' name='{{formScope.name}}[]' ng-model='files' id='{{formScope.id}}' enctype='multipart/form-data' multiple><label for",
+                template: "<label for='{{formScope.id}}' class='btn btn-default'>Selecionar archivo</label><input type='file' class = 'hide' name='{{formScope.name}}[]' ng-model='files' id='{{formScope.id}}' enctype='multipart/form-data' multiple><input type='hidden' name='recipients[]' value='{{formScope.email}}' ng-model='model.email'><input type='hidden' name='subject' value='Sing Document' ng-model='model.email'>",
 
                 scope: {
                     ngJqueryFormAction:     '@',
@@ -19,28 +19,41 @@
                     ngJqueryFormSend:       '=',
                     ngJqueryFormFiles:      '=',
                     ngJqueryFormProgress:   '=',
-                    ngJqueryFormError:      '='
+                    ngJqueryFormError:      '=',
+                    ngJqueryFormSuccess:    '=',
+                    ngJqueryFormHeader:     '=',
+                    ngJqueryFormEmail:      '='
                 },
-                link: function ($scope, $element, $attrs){
-                    var url, uploadProgress, writeError, writeSuccess;
+
+                link: function ($scope, $element, $attrs, $watch){
+                    var url, progress, writeError, writeSuccess, clearFiles;
 
                     $scope.formScope = {};
+
                     $element.change( function () {
                         if ( $attrs.ngJqueryFormFiles ) {
                             $scope.$apply(function() {
                                 angular.forEach($element.formToArray(), function (file) {
-                                    $scope.ngJqueryFormFiles.push(file.value);
+                                    console.log(file);
+                                    if ( file.type ===  "file" ) {
+                                        if ( file.value.type === "application/pdf" ) { 
+                                            $scope.ngJqueryFormFiles.push(file.value);
+                                        } else {
+                                            if( $attrs.ngJqueryFormError ) {
+                                                $scope.ngJqueryFormError('', {'code' : -601});
+                                            }
+                                        }
+                                    }
                                 })
                             })
                         }
                     });
 
-
                     if ( $attrs.ngJqueryFormAction ) {
                         url = $scope.ngJqueryFormAction;
                     }
                     if ( $attrs.ngJqueryFormProgress ) {
-                        $scope.ngJqueryFormProgress = 0;
+                        $scope.ngJqueryFormProgress ;
                     }   
                     if ( $attrs.ngJqueryFormFiles ) {
                         $scope.ngJqueryFormFiles = [];
@@ -60,49 +73,77 @@
                     // con nuestra funcion
                     if ( $attrs.ngJqueryFormSend ) {
                         $scope.ngJqueryFormSend = function () {
-                            
-                            $element.ajaxForm({
-                                url: url,
-                                type: 'POST',
-                                uploadProgress: uploadProgress,
-                                success: function (response, status) {
-                                    writeSuccess(response, status);
-                                },
-                                error: function (response, status) {
-                                    writeError(response, status);
-                                }
-                            });
+                            if ( $attrs.ngJqueryFormEmail ) {
+                                $scope.ngJqueryFormProgress = true;
+                                $scope.formScope.email = $scope.ngJqueryFormEmail;
 
-                            $element.submit();
+                                if ( $scope.formScope.email !== "" && $scope.ngJqueryFormFiles.length > 0) {
+                                    setTimeout(function (){
+                                        $element.ajaxForm({
+                                            url: url,
+                                            type: 'POST',
+                                            headers: $scope.ngJqueryFormHeader,
+                                            beforeSubmit: function (){
+                                                progress(true);
+                                                return true;
+                                            },
+                                            uploadProgress: function (event, position, total, percentComplete) {
+                                                console.log("el progress es");
+                                                console.log(total);
+                                                console.log(position);
+                                                console.log(percentComplete);
+                                                progress(percentComplete)
+                                                
+                                            },
+                                            success: function (response, status) {
+                                                writeSuccess(response, status);
+                                            },
+                                            error: function (response, status) {
+                                                writeError(response, status);
+                                            }
+                                        });
+                                        $element.submit();
+                                    }, 500);
+                                } else {
+                                    if( $attrs.ngJqueryFormError ) {
+                                        if($scope.ngJqueryFormFiles.length === 0){
+                                            $scope.ngJqueryFormError('', {'code' : 405});
+                                        } else {
+                                            $scope.ngJqueryFormError('', {'code' : 404});
+                                        }
+                                    }
+                                }
+                            }
                         }
                     }
 
                     if ($attrs.ngJqueryFormClear){
-                        $scope.ngJqueryFormClear = function () {
-                            $element.resetForm();
-                            $scope.ngJqueryFormFiles = [];
-                        }
+                        $scope.ngJqueryFormClear = clearFiles;
                     }
 
-                    uploadProgress = function (event, position, total, percentComplete) {
+                    clearFiles = function () {
+                        $element.resetForm();
+                        $scope.ngJqueryFormFiles = [];
+                    }
+                    progress = function (percentComplete) {
                         $scope.$apply(function () {
-                            if( $scope.ngJqueryForm )
                             $scope.ngJqueryFormProgress = percentComplete;
+                            console.log($scope.ngJqueryFormProgress);
                         })
                     };
 
-                    writeError = function (msg) {
+                    writeError = function (response, status) {
                         $scope.$apply(function () {
                             if( $attrs.ngJqueryFormError ) {
-                                $scope.ngJqueryFormError(arguments);
+                                $scope.ngJqueryFormError(response, status);
                             }
                         })
                     };
 
-                    writeSuccess = function () {
+                    writeSuccess = function (response, status) {
                         $scope.$apply(function () {
                             if( $attrs.ngJqueryFormSuccess ) {
-                                $scope.ngJqueryFormSuccess(arguments);
+                                $scope.ngJqueryFormSuccess(response, status);
                             }
                         })
                     } 
